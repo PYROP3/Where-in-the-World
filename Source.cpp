1235e339cff66574a07bd1e064e5341c0a2f06f4
BrunoMarini <bgmarini@hotmail.com>
2017-10-26 18:29:45 -0200

    kkk eae man

--- Source.cpp
+++ Source.cpp
@@ -51,7 +51,6 @@ int main() {
 	char string[30], resposta;
 	int i;
 	bool valido = true, login = true;
-
 	char ajuda = 'N';
 
 	do {
@@ -168,9 +167,6 @@ int main() {
 
 					printf("\n\tOpção 1. Cadastro novo Jogador:\n\n\tDigite seu primeiro nome: ");
 					scanf("%s", jogador.nome);
-
-					printf("\n\tDigite seu login: ");
-					scanf("%s", jogador.login);
 
 					do  //INICIO VERIFICAÇÃO NOME EXISTENTE
 					{
